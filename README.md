425159c96cdcf6afe1464db68358af5e4b487305
fahahm <fahaadahmed05@gmail.com>
2025-08-08 23:35:13 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+<div align="center">
+    <a href="https://url.com"><h1 align="center">url.com</h1></a>
+
+My personal website, built with [Next.js](https://nextjs.org/), [Tailwind CSS](https://tailwindcss.com/), [Upstash](https://upstash.com), [Contentlayer](https://www.contentlayer.dev/) and deployed to [Vercel](https://vercel.com/).
+
+</div>
+
+<br/>
+
+## Running Locally
+
+```sh-session
+git clone https://github.com/fahaad05/personal-portfolio.git
+```
+
+Then install dependencies and run the development server:
+
+```sh-session
+pnpm install
+pnpm dev
+```
---
+++ app/layout.tsx
@@ -0,0 +1,72 @@
+import { Metadata } from "next";
+import { Inter } from "next/font/google";
+import LocalFont from "next/font/local";
+import "../global.css";
+import { Analytics } from "./components/analytics";
+
+export const metadata: Metadata = {
+  title: {
+    default: "url.com", //TODO Insert my URL
+    template: "%s | url.com", //TODO Insert my URL
+  },
+  description: "Software Engineer and Master's student in Cybersecurity",
+  openGraph: {
+    title: "url.com",
+    description:
+      "Software Engineer and Master's student in Cybersecurity",
+    url: "url.com",
+    siteName: "url.com",
+    images: [
+      {
+        url: "https://url.com/og.png",
+        width: 1920,
+        height: 1080,
+      },
+    ],
+    locale: "it-CH",
+    type: "website",
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
+  icons: {
+    shortcut: "/favicon.png",
+  },
+};
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+});
+
+const calSans = LocalFont({
+  src: "../public/fonts/CalSans-SemiBold.ttf",
+  variable: "--font-calsans",
+});
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" className={[inter.variable, calSans.variable].join(" ")}>
+      <head>
+        <Analytics />
+      </head>
+      <body
+        className={`bg-black ${process.env.NODE_ENV === "development" ? "debug-screens" : undefined
+          }`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
